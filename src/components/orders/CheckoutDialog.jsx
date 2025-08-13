5838c75613b75cda165c3996cdde6307e859c04d
ryus3 <jawhra.iq@gmail.com>
2025-08-13 20:01:03 +0300

    إضافة المكتبات المطلوبة

--- src/components/orders/CheckoutDialog.jsx
+++
@@ -1,6 +0,0 @@
-// This component is no longer used and can be considered for removal in the future.
-// The functionality has been consolidated into QuickOrderDialog and QuickOrderContent.
-import React from 'react';
-
-const CheckoutDialog = () => null;
-export default CheckoutDialog;
--- src/components/quick-order/ProductSelector.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-// This component is no longer used and can be considered for removal in the future.
-// The functionality has been consolidated into QuickOrderDialog and QuickOrderContent.
-const ProductSelector = () => null;
-export default ProductSelector;
